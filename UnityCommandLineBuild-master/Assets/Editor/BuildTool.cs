0aa1a970ccf9a341c47e9be1c4fa034562db1e64
LiaoYiZheng <410121038@ems.ndhu.edu.tw>
2018-04-13 11:09:32 +0800

    TryApk

--- UnityCommandLineBuild-master/Assets/Editor/BuildTool.cs
+++ UnityCommandLineBuild-master/Assets/Editor/BuildTool.cs
@@ -75,4 +75,37 @@ public class BuildTool
 
         return extension;
     }
+
+    [MenuItem("BuildTool/APKBuild")]
+    public static void APKBuild()
+    {
+        BuildTarget buildTarget = BuildTarget.Android;
+        // 切换到 Android 平台
+        EditorUserBuildSettings.SwitchActiveBuildTarget(buildTarget);
+
+        // keystore 路径, G:\keystore\one.keystore
+        /*PlayerSettings.Android.keystoreName = "G:\\keystore\\one.keystore";
+        // one.keystore 密码
+        PlayerSettings.Android.keystorePass = "123456";
+
+        // one.keystore 别名
+        PlayerSettings.Android.keyaliasName = "bieming1";
+        // 别名密码
+        PlayerSettings.Android.keyaliasPass = "123456";*/
+
+        List<string> levels = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            // 获取有效的 Scene
+            levels.Add(scene.path);
+        }
+
+        // 打包出 APK 名
+        string apkName = string.Format("./{0}.apk", "Test");
+        // 执行打包
+        string res = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
+
+        AssetDatabase.Refresh();
+    }
 }
